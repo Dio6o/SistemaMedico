c3b8d2c603d13bc37915af850eeafedf09ccdd91
kauanscheidt <okauanscheidt@gmail.com>
2025-10-23 19:26:38 -0300

    criação classe pessoa

---
+++ arquivosJava/Pessoa.java
@@ -0,0 +1,34 @@
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null) {
+            this.nome = nome;
+        }
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        if (cpf != null) {
+            this.cpf = cpf;
+        }
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Pessoa{");
+        sb.append("nome='").append(getNome()).append('\'');
+        sb.append(", cpf='").append(getCpf()).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
