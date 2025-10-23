fda3033d2336e76b41a99505cfb1c4ec40eb8b58
kauanscheidt <okauanscheidt@gmail.com>
2025-10-23 19:54:13 -0300

    Adição de novas classes

---
+++ arquivosJava/Atendente.java
@@ -0,0 +1,36 @@
+public class Atendente extends Pessoa {
+
+    private String setor;
+    private Integer idFuncionario;
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        if (setor != null) {
+            this.setor = setor;
+        }
+    }
+
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncinario(Integer idFuncionario) {
+        if ((idFuncionario != null) && (idFuncionario > 0)) {
+            this.idFuncionario = idFuncionario;
+        }
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Atendente{");
+        sb.append("setor='").append(getSetor()).append('\'');
+        sb.append(", idFuncionario=").append(getIdFuncionario());
+        sb.append(", nome='").append(getNome()).append('\'');
+        sb.append(", cpf='").append(getCpf()).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ arquivosJava/Paciente.java
@@ -0,0 +1,37 @@
+public class Paciente extends Pessoa{
+
+    private String tipoSangue;
+    private String Alergias;
+
+    public String getTipoSangue() {
+        return tipoSangue;
+    }
+
+    public void setTipoSangue(String tipoSangue) {
+        if (tipoSangue != null) {
+            this.tipoSangue = tipoSangue;
+        }
+    }
+
+    public String getAlergias() {
+        return Alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        if (alergias != null) {
+            Alergias = alergias;
+        }
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Paciente{");
+        sb.append("tipoSangue='").append(getTipoSangue()).append('\'');
+        sb.append(", Alergias='").append(getAlergias()).append('\'');
+        sb.append(", alergias='").append(getAlergias()).append('\'');
+        sb.append(", nome='").append(getNome()).append('\'');
+        sb.append(", cpf='").append(getCpf()).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
